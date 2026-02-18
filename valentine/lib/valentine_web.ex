0a6133a96a36848bac333c0832852e10003a01ea
Iustin Dumitru <132302893+idumitru-cds@users.noreply.github.com>
2026-02-18 17:33:20 -0500

    Fix 404 for /.well-known/security.txt in prod (#96)
    * Add security.txt for vulnerability reporting
    
    * Add '.well-known' to static paths
    
    Add .well-known to ValentineWeb.static_paths/0 so Phoenix Plug.Static can serve /.well-known/security.txt. Without this change, the file existed in priv/static but was blocked by the only: allowlist, resulting in 404 in production.

--- valentine/lib/valentine_web.ex
+++ valentine/lib/valentine_web.ex
@@ -17,7 +17,7 @@ defmodule ValentineWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt primer_live)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt primer_live .well-known)
 
   def router do
     quote do
